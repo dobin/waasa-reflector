044f15b6ff49db23cd5cf6008358cc3993b2c54d
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2023-10-06 15:58:20 +0200

    refactor: new api

--- app/views.py
+++ app/views.py
@@ -13,6 +13,8 @@ import base64
 
 views = Blueprint('views', __name__)
 
+WAAZA_EXT = ".waaza_test"
+
 def get_mimetype(filename):
     mime_type, encoding = mimetypes.guess_type(filename)
     return mime_type
@@ -26,7 +28,7 @@ def index():
 # Filename in URL
 # Filename in Content-Disposition
 # Correct MimeType 
-@views.route("/simple/<filename>")
+@views.route("/standard/<filename>")
 def file_simple(filename):
     if secure_filename(filename) != filename:
         return "Error", 500
@@ -39,25 +41,10 @@ def file_simple(filename):
 
 
 # Filename in URL
-# Filename NOT in Content-Disposition
-# Correct MimeType
-@views.route("/2/<filename>")
-def file_2(filename):
-    if secure_filename(filename) != filename:
-        return "Error", 500
-    
-    response = make_response(b"data")
-    mimeStr = get_mimetype(filename)
-    response.headers['Content-Type'] = mimeStr
-    response.headers['Content-Disposition'] = "attachment"
-    return response
-
-
-# Filename in URL
 # Filename in Content-Disposition
 # NOT Correct MimeType
-@views.route("/3/<filename>")
-def file_3(filename):
+@views.route("/nomime/<filename>")
+def file_phase1(filename):
     if secure_filename(filename) != filename:
         return "Error", 500
     
@@ -70,15 +57,13 @@ def file_3(filename):
 # NO Filename in URL
 # Filename in Content-Disposition
 # NOT Correct MimeType
-@views.route("/4/<filename_b64>")
-def file_4(filename_b64):
+@views.route("/nomimenofilename/<filename_b64>")
+def file_phase2(filename_b64):
     filename = base64.b64decode(filename_b64).decode('utf-8')
-    print("A: " + filename)
     if secure_filename(filename) != filename:
         return "Error", 500
-    
+
     response = make_response(b"data")
     response.headers['Content-Type'] = "octet/stream"
     response.headers['Content-Disposition'] = "attachment; filename={}".format(filename)
     return response
-
